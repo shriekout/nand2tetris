8eeaeef5d20a69398d372be92751709c1edbffeb
Kim Jincheol <shriekout@gmail.com>
2025-04-29 20:45:45 +0900

    Solve 4/mult

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @R2
+    M=0
+
+(LOOP)
+    @R0
+    M=M-1
+    D=M
+    @END
+    D;JLT
+    
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
