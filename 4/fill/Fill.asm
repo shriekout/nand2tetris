5fad7c5c036f254b511b8be8e0e590857adf5d2e
Kim Jincheol <shriekout@gmail.com>
2025-04-29 21:25:17 +0900

    Solve 4

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,58 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+    @pos
+    M=0
+
+(LOOP)
+    @KBD
+    D=M
+
+    @WHITE
+    D;JEQ
+
+    @BLACK
+    D;JGT
+
+(WHITE)
+    @pos
+    D=M
+
+    @LOOP
+    D;JLT
+
+    @SCREEN
+    A=A+D
+    M=0
+
+    @pos
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(BLACK)
+    @pos
+    D=M
+    @8191
+    D=D-A
+
+    @LOOP
+    D;JGE
+
+    @pos
+    D=M
+
+    @SCREEN
+    A=A+D
+    M=-1
+
+    @pos
+    M=M+1
+    
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
