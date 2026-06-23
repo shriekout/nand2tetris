8954eaefff28ec931b6bcb8c98017598a2263ccb
Kim Jincheol <shriekout@gmail.com>
2026-06-23 18:31:17 +0900

    Complete 4 Mult.asm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,29 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+    // R2 = 0
+    @R2
+    M=0
+(LOOP)
+    // if (R1 == 0) goto END
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=M+D
+
+    // R1 = R1 - 1
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
